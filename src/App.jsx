6d93ec7c52f5c55e0be4e0c3dea683d04d05090f
Peywan Sabir <peywan1999@icloud.com>
2024-04-29 12:38:27 +0200

    added usestate for onclick x

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-function Square({value}) {
+import {useState} from 'react';
+
+function Square() {
+    const [value, setValue] = useState(null);
+
     function handleClick() {
-        console.log('clicked!');
+        setValue("X");
     }
 
-    export default function Board() {
-        return (
-            <>
-                return (
-                <button
-                    className="square"
-                    onClick={handleClick}
-                >
-                    {value}
-                </button>
-                );
-            </>
-        );
-    }
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}
+
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+        </>
+    );
+}
